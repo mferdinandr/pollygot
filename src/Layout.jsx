7e24f31ccc862417f9b1bd67e0c4f527a9ea91e8
mferdinandr <mferdinr@gmail.com>
2025-03-20 06:23:44 +0700

    feat: update layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,8 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-md h-[90vh] mx-auto my-10 flex flex-col flex-1 shadow-md shadow-gray-200 fixed left-1/2 -translate-x-1/2">
-      <img
-        src="/public/header.png"
-        alt="header image"
-        className="rounded-t-lg"
-      />
+    <div className="w-md h-[90vh] mx-auto my-10 flex flex-col flex-1 shadow-lg shadow-gray-300 fixed left-1/2 -translate-x-1/2">
+      <img src="/header.png" alt="header image" className="rounded-t-lg" />
       <div className="border-2 border-black mx-3 my-5 rounded-lg h-full overflow-hidden">
         {children}
       </div>
